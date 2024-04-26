dee06dbaa81b56803ceae001a3db2f8ee3ec3c05
Uriel Sautron <uriel.sautron@gmail.com>
2024-04-26 15:13:49 +0200

    - regex validation margin

--- front/src/modules/timesStops/TimesStops.tsx
+++ front/src/modules/timesStops/TimesStops.tsx
@@ -3,33 +3,27 @@ import React, { useMemo, useState, useEffect } from 'react';
 import type { Position } from 'geojson';
 import {
   DataSheetGrid,
-  textColumn,
   keyColumn,
-  intColumn,
   type Column,
   checkboxColumn,
+  createTextColumn,
+  type CellProps,
 } from 'react-datasheet-grid';
 import { useTranslation } from 'react-i18next';
 
-import {
-  osrdEditoastApi,
-  type PathfindingResult,
-  type PathProperties,
-  type PathWaypoint,
-  type TrackRange,
-} from 'common/api/osrdEditoastApi';
+import type { ManageTrainSchedulePathProperties } from 'applications/operationalStudies/views/v2/ManageTrainScheduleV2';
+import { type PathProperties } from 'common/api/osrdEditoastApi';
 import DATATEST from 'common/Pathfinding/dataTest';
-import { useInfraID } from 'common/osrdContext';
 
 type TimesStopsProps = {
-  path: PathfindingResult;
+  pathProperties?: ManageTrainSchedulePathProperties;
 };
 
 type SuggestedOP = {
   prId: string;
-  name: string | null;
+  name: string;
   uic?: number;
-  ch: string | null;
+  ch: string;
   ch_long_label?: string;
   ch_short_label?: string;
   ci?: number;
@@ -47,13 +41,15 @@ type SuggestedOP = {
   deleted?: boolean;
   arrival?: string | null;
   locked?: boolean;
-  stop_for?: string | null;
+  stop_for?: string;
 };
+
+const marginRegExValidation = /^\d+(\.\d+)?%$|^\d+(\.\d+)?min\/km$/;
 
 const formatOPsToSuggestOPs = (
   operationalPoints: PathProperties['operational_points']
-): SuggestedOP[] => {
-  const suggestedOPs: SuggestedOP[] = [];
+): PathWaypointColumn[] => {
+  const suggestedOPs: PathWaypointColumn[] = [];
   operationalPoints?.forEach((op) => {
     const { extensions, id, part, position } = op;
     if (!extensions) return;
@@ -76,67 +72,94 @@ const formatOPsToSuggestOPs = (
       track,
       positionOnPath,
       coordinates: [0, 0],
+      arrival_time: null,
+      departure_time: null,
+      duration: 0,
+      reception_on_closed_signal: false,
+      theoretical_margin: null,
+      isMarginValid: true,
     });
   });
   return suggestedOPs;
 };
-const TimesStops = ({ path }: TimesStopsProps) => {
-  const { t } = useTranslation('timesStops');
-  const infraID = useInfraID();
 
-  const [postPathProperties] =
-    osrdEditoastApi.endpoints.postV2InfraByInfraIdPathProperties.useMutation();
+type PathWaypointColumn = SuggestedOP & {
+  duration: number;
+  arrival_time: string | null;
+  departure_time: string | null;
+  reception_on_closed_signal: boolean;
+  theoretical_margin: string | null;
+  isMarginValid: boolean;
+};
+
+const TimeComponent = ({
+  rowData,
+  setRowData,
+  rowIndex,
+  columnIndex,
+}: CellProps<string | null, any>) => (
+  <input
+    id={`time-${rowIndex}-${columnIndex}`}
+    type="time"
+    value={rowData!}
+    onChange={(e) => {
+      console.log('rowData:', rowData);
+      setRowData(e.target.value);
+    }}
+    className="dsg-input"
+    step="1"
+  />
+);
+
+const timeColumn: Partial<Column<string | null, any, string>> = {
+  component: TimeComponent,
+};
+
+const TimesStops = ({ pathProperties }: TimesStopsProps) => {
+  const { t } = useTranslation('timesStops');
+
   const [timesStopsSteps, setTimesStopsSteps] = useState<Partial<PathWaypointColumn>[] | undefined>(
     undefined
   );
 
   useEffect(() => {
-    postPathProperties({
-      infraId: infraID as number,
-      pathPropertiesInput: {
-        track_ranges: path.track_section_ranges as TrackRange[],
-      },
-    })
-      .unwrap()
-      .then((result) => {
-        const suggestedOPs = formatOPsToSuggestOPs(DATATEST);
-        setTimesStopsSteps(suggestedOPs);
-      });
-  }, [path]);
-
-  type PathWaypointColumn = SuggestedOP & {
-    duration: number | null;
-    arrival_time: string | null;
-    departure_time: string | null;
-    reception_on_closed_signal: boolean;
-    theoretical_margin: string | null;
-  };
+    // if (!pathProperties) return;
+    // const suggestedOPs = formatOPsToSuggestOPs(pathProperties.operational_points);
+    const suggestedOPs = formatOPsToSuggestOPs(DATATEST);
+    setTimesStopsSteps(suggestedOPs);
+  }, [pathProperties]);
 
   const columns: Column<PathWaypointColumn>[] = useMemo(
     () => [
       {
-        ...keyColumn<PathWaypointColumn, 'name'>('name', textColumn),
+        ...keyColumn<PathWaypointColumn, 'name'>('name', createTextColumn<string>()),
         title: t('name'),
         disabled: true,
       },
       {
-        ...keyColumn<PathWaypointColumn, 'ch'>('ch', textColumn),
+        ...keyColumn<PathWaypointColumn, 'ch'>('ch', createTextColumn()),
         title: 'Ch',
         disabled: true,
         grow: 0.1,
       },
       {
-        ...keyColumn<PathWaypointColumn, 'arrival_time'>('arrival_time', textColumn),
+        ...keyColumn<PathWaypointColumn, 'arrival_time'>('arrival_time', timeColumn),
         title: t('arrival_time'),
-        grow: 0.5,
+        grow: 0.6,
       },
       {
-        ...keyColumn<PathWaypointColumn, 'departure_time'>('departure_time', textColumn),
+        ...keyColumn<PathWaypointColumn, 'departure_time'>('departure_time', timeColumn),
         title: t('departure_time'),
-        grow: 0.5,
+        grow: 0.6,
       },
       {
-        ...keyColumn<PathWaypointColumn, 'duration'>('duration', intColumn),
+        ...keyColumn<PathWaypointColumn, 'duration'>(
+          'duration',
+          createTextColumn<number>({
+            continuousUpdates: false,
+            alignRight: true,
+          })
+        ),
         title: t('stop_time'),
       },
       {
@@ -147,22 +170,56 @@ const TimesStops = ({ path }: TimesStopsProps) => {
         title: t('reception_on_closed_signal'),
       },
       {
-        ...keyColumn<PathWaypointColumn, 'theoretical_margin'>('theoretical_margin', textColumn),
+        ...keyColumn<PathWaypointColumn, 'theoretical_margin'>(
+          'theoretical_margin',
+          createTextColumn({
+            continuousUpdates: false,
+            alignRight: true,
+            placeholder: t('theoretical_margin_placeholder'),
+            formatBlurredInput: (value) => {
+              if (!value) return '';
+              if (!marginRegExValidation.test(value))
+                return `${value}${t('theoretical_margin_placeholder')}`;
+              return value;
+            },
+          })
+        ),
         title: t('theoretical_margin'),
+        disabled: ({ rowIndex }) => rowIndex === -1,
       },
     ],
-    [t]
+    [t, pathProperties]
   );
 
   return (
     <DataSheetGrid
-      columns={columns}
+      columns={columns as Partial<Column<Partial<PathWaypointColumn>, any, any>>[]}
       value={timesStopsSteps}
-      onChange={(e) => {
+      onChange={(e, [op]) => {
+        console.log('op:', op);
+        console.log('e test:', e);
+        if (
+          e[`${op.fromRowIndex}`].theoretical_margin &&
+          !marginRegExValidation.test(e[`${op.fromRowIndex}`].theoretical_margin!)
+        ) {
+          e[`${op.fromRowIndex}`].isMarginValid = false;
+        } else {
+          e[`${op.fromRowIndex}`].isMarginValid = true;
+        }
+
         setTimesStopsSteps(e as PathWaypointColumn[]);
       }}
       lockRows
       height={600}
+      rowClassName={({ rowData }) =>
+        rowData.departure_time || rowData.arrival_time || rowData.duration ? 'activeRow' : ''
+      }
+      rowKey={({ rowData }) => `${rowData.prId}`}
+      cellClassName={({ rowData, columnId }) => {
+        if (columnId === 'theoretical_margin' && !(rowData as PathWaypointColumn).isMarginValid)
+          return 'invalidCell';
+        return '';
+      }}
     />
   );
 };
